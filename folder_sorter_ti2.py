683bf55c758677ccdac9e5df0e4b32338bf729cd
Alex <yarimov@ya.ru>
2023-10-09 14:26:22 +0300

    Добавлен файл requirements.txt

--- folder_sorter_ti2.py
+++ folder_sorter_ti2.py
@@ -13,6 +13,7 @@ customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 # Основной цвет оформления приложения: "blue" (standard), "green", "dark-blue"
 
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
